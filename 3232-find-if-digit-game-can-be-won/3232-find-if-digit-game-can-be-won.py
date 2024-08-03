d6e3e21c351b4f04f8fdc31ede813672657fc1dc
Heena Gupta <92094543+Heena0001@users.noreply.github.com>
2024-08-03 21:46:41 +0530

    Time: 52 ms (80.09%), Space: 16.5 MB (25.42%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        single_digit_sum = 0
+        double_digit_sum = 0
+
+        for num in nums:
+            if 0 <= num <= 9:
+                single_digit_sum += num
+            elif 10 <= num <= 99:
+                double_digit_sum += num
+        if (single_digit_sum == double_digit_sum):
+            return False
+        else:
+            return True
