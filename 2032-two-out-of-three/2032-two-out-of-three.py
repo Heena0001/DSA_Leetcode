21de07d321374eb89a95da9bb0537e6a30741c88
Heena Gupta <92094543+Heena0001@users.noreply.github.com>
2024-08-22 23:00:18 +0530

    Time: 65 ms (61.03%), Space: 16.5 MB (54.46%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        ans=[]
+        for i in nums1:
+            if i in nums2 or i in nums3:
+                if(i not in ans):
+                    ans.append(i)
+        for i in nums2:
+            if i in nums3 :
+                if(i not in ans):
+                    ans.append(i)
+        return ans
